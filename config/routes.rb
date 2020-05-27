fed3b43ee660f271cb25f0278d17558fda0ce334
Roberto Barros <roberto.f.barros@gmail.com>
2020-05-27 18:46:11 -0300

    livecode

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :gardens do
+    resources :plants, only: [:new, :create]
+  end
+
+  resources :plants, only: :destroy
+
+
+  root to: 'gardens#index'
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+
+Garden.create!(name: 'Meu Jardim', banner_url: 'https://images.unsplash.com/photo-1573140087145-1447f1ba2302')
+Garden.create!(name: 'Outro Jardim', banner_url: 'https://images.unsplash.com/photo-1534710961216-75c88202f43e')
